44e4862130bbecda2e69a85bac1d653307395f18
ivanmarchenko <marchenkoivanu@gmail.com>
2020-10-22 18:22:40 +0300

    5

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,7 +4,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('', posts_list, name='blog'),
+    path('', posts_list, name='posts_list_url'),
     # <str:slug> - slug равен имени slug в параметрах функции post_detail(..., slug)
     path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/<str:slug>/', tag_detail, name='tag_detail_url')
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Tag
 
 def posts_list(request):
     n = ['Oleg', 'Masha', 'Olya', 'Peter']
@@ -10,3 +10,11 @@ def posts_list(request):
 def post_detail(request, slug): 
     post = Post.objects.get(slug__iexact=slug)
     return render(request, 'blogapp/post_detail.html', context={'post': post})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blogapp/tags_list.html', context={'tags': tags})
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blogapp/tag_detail.html', context={'tag':tag})
